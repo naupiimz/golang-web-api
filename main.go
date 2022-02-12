77705ce7ba7b35cf222daa1b396c54cd0f10a44e
naupiimz <naupiimz@gmail.com>
2022-02-12 18:57:00 +0700

    initialize database

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"fmt"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "db_golang"
 )
 
 func main() {
@@ -13,6 +24,27 @@ func main() {
 
 	r.GET("/product/:id", productHandler)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func rootHandler(c *gin.Context) {
